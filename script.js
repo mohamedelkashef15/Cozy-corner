bfbff21e711fb29fac0f78de25953a321f77550d
mohamedelkashef <mohamedelkashef38@gmail.com>
2026-06-18 12:55:55 +0300

    update hero section

---
+++ script.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".hero-slider", {
+  loop: true,
+
+  speed: 1000,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
